67740d7aba423f88d23f9b1cf5dd8c7b2a55fc43
Emran <emran86133@gmail.com>
2024-02-26 20:29:30 +0600

    grand total are added

--- script/script.js
+++ script/script.js
@@ -1,16 +1,20 @@
 let totalPrice = 0;
 const cards = document.querySelectorAll(".kbd");
 // console.log(cards);
+
 for (let index = 0; index < cards.length; index++) {
     const card = cards[index];
+
 
     // console.log(card);
     card.addEventListener("click", function (event) {
         event.target.disabled = true;
         event.target.style.background = 'green';
 
+
         // console.log('just-clicks');
         const key = event.target.innerText;
+
 
         // console.log(key);
 
@@ -19,15 +23,13 @@ for (let index = 0; index < cards.length; index++) {
         // console.log(price);
         const seatContainer = document.getElementById('seat-container');
 
-
+        //create a div thats contain seat information
         const div = document.createElement('div');
         div.classList.add('flex', 'justify-between')
         const busSeat = document.getElementById('bus-seat')
         const h4 = document.createElement("h4");
         h4.innerText = key;
         div.appendChild(h4);
-
-
 
         const seatClass = document.getElementById('class');
         const h6 = document.createElement("p");
@@ -44,7 +46,9 @@ for (let index = 0; index < cards.length; index++) {
 
         //total price
         totalPrice += price;
+
         document.getElementById("total-price").innerText = totalPrice;
+        document.getElementById("grand-total").innerText = totalPrice;
         const seatIncrease = document.getElementById('seat-increase');
         const increaseSeat = parseFloat(seatIncrease.innerText)
         document.getElementById('seat-increase').innerText = increaseSeat + 1;
@@ -52,11 +56,29 @@ for (let index = 0; index < cards.length; index++) {
         const seatDecrease = document.getElementById('total-seats');
         const decreaseSeat = parseFloat(seatDecrease.innerText)
         document.getElementById('total-seats').innerText = decreaseSeat - 1;
+
         // console.log(totalPrice);
     });
 }
 
 
+
+const btn = document.getElementById('btn');
+btn.addEventListener("click", function () {
+    // console.log('click');
+
+    //get the value from input
+    const inputField = document.getElementById('input-field').value;
+    // console.log(inputField);
+    const code = inputField.split(" ").join("").toUpperCase();
+    if (code === 15 % OFF) {
+        return true;
+
+    }
+    else {
+        return false;
+    }
+})
 
 
 
